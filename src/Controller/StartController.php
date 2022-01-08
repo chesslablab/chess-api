a184df3743123d81bb2e0b127c2ccc2c18a3757b
programarivm <info@programarivm.com>
2022-01-08 11:25:42 +0100

    Set up the StartController by playing a game

--- src/Controller/StartController.php
+++ src/Controller/StartController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use Chess\Player;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StartController extends AbstractController
 {
-    #[Route('/start', name: 'start')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/StartController.php',
-        ]);
+        $params = json_decode($request->getContent(), true);
+
+        $player = new Player($params['movetext']);
+
+        $board = $player->play()->getBoard();
+
+        $status = (object) [
+            'castling' => $board->getCastling(),
+            'isCheck' => $board->isCheck(),
+            'isMate' => $board->isMate(),
+            'movetext' => $board->getMovetext(),
+            'turn' => $board->getTurn(),
+        ];
+
+        return $this->json($status);
     }
 }
