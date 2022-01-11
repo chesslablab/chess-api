ac98c022ee491d41ccba5757441ca39403c5dbd6
programarivm <info@programarivm.com>
2022-01-11 11:20:02 +0100

    Failed test foo()

---
+++ tests/integration/DatabaseTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ChessApi\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class DatabaseTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function foo()
+    {
+        // TODO
+
+        $this->assertTrue(false);
+    }
+}
