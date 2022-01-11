be89869309d57a8a45ede7a5f07235ac8249fd5a
programarivm <info@programarivm.com>
2022-01-11 23:41:36 +0100

    Cleaned up PlayController

--- src/Controller/PlayController.php
+++ src/Controller/PlayController.php
@@ -9,7 +9,6 @@ use Chess\Exception\UnknownNotationException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class PlayController extends AbstractController
