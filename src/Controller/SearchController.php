274df427c6becca312772141a9ae59620735635d
programarivm <info@programarivm.com>
2022-08-24 23:34:50 +0200

    Implemented SearchController

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ChessApi\Controller;
+
+use ChessApi\Pdo;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SearchController extends AbstractController
+{
+    public function index(Request $request): Response
+    {
+        $conf = [
+            'driver' => $_ENV['DB_DRIVER'],
+            'host' => $_ENV['DB_HOST'],
+            'database' => $_ENV['DB_DATABASE'],
+            'username' => $_ENV['DB_USERNAME'],
+            'password' => $_ENV['DB_PASSWORD'],
+        ];
+
+        $params = json_decode($request->getContent(), true);
+
+        $sql = 'SELECT * FROM players WHERE ';
+        $values = [];
+        foreach ($params as $key => $val) {
+            $sql .= "$key LIKE :$key AND ";
+            $values[] = [
+                'param' => ":$key",
+                'value' => '%'.$val.'%',
+                'type' => \PDO::PARAM_STR,
+            ];
+        }
+        str_ends_with($sql, 'WHERE ')
+            ? $sql = substr($sql, 0, -6)
+            : $sql = substr($sql, 0, -4);
+        $sql .= 'ORDER BY RAND() LIMIT 25';
+
+        $arr = Pdo::getInstance($conf)
+            ->query($sql, $values)
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        if ($arr) {
+            return $this->json($arr);
+        }
+
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_NO_CONTENT);
+
+        return $response;
+    }
+}
