d8726a6874b966ed31525ecbc93f5574c338b454
programarivm <info@programarivm.com>
2022-08-25 21:06:45 +0200

    Refactored GrandmasterController

--- src/Controller/GrandmasterController.php
+++ src/Controller/GrandmasterController.php
@@ -13,32 +13,20 @@ class GrandmasterController extends AbstractController
     {
         $params = json_decode($request->getContent(), true);
 
-        $sql = "SELECT * FROM players WHERE movetext LIKE '{$params['movetext']}%'";
+        $sql = "SELECT * FROM players WHERE movetext LIKE :movetext";
+
+        $values[] = [
+            'param' => ':movetext',
+            'value' => $params['movetext'].'%',
+            'type' => \PDO::PARAM_STR,
+        ];
 
         $arr = Pdo::getInstance($this->getParameter('pdo'))
-            ->query($sql)
+            ->query($sql, $values)
             ->fetchAll(\PDO::FETCH_ASSOC);
 
         if ($arr) {
-            shuffle($arr);
-            $game = $arr[0];
-            $moves = array_filter(
-                explode(' ', str_replace($params['movetext'], '', $game['movetext']))
-            );
-            $current = explode('.', current($moves));
-            return $this->json([
-                'move' => isset($current[1]) ? $current[1] : $current[0],
-                'game' => [
-                    'Event' => $game['Event'],
-                    'Site' => $game['Site'],
-                    'Date' => $game['Date'],
-                    'White' => $game['White'],
-                    'Black' => $game['Black'],
-                    'Result' => $game['Result'],
-                    'ECO' => $game['ECO'],
-                    'movetext' => $game['movetext'],
-                ],
-            ]);
+            return $this->json($arr);
         }
 
         $response = new Response();
