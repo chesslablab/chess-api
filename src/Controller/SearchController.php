d57bf679b309374685b3f3dc7842431e13fbef6f
programarivm <info@programarivm.com>
2022-08-28 10:58:33 +0200

    Optimized SQL query

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -9,23 +9,47 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends AbstractController
 {
+    const SEARCH_LIKE = [
+        'Event',
+        'Date',
+        'White',
+        'Black',
+    ];
+
+    const SEARCH_AND = [
+        'ECO',
+        'Result',
+    ];
+
     public function index(Request $request): Response
     {
         $params = json_decode($request->getContent(), true);
 
         $sql = 'SELECT * FROM players WHERE ';
         $values = [];
+
         foreach ($params as $key => $val) {
-            $sql .= "$key LIKE :$key AND ";
-            $values[] = [
-                'param' => ":$key",
-                'value' => '%'.$val.'%',
-                'type' => \PDO::PARAM_STR,
-            ];
+            if (in_array($key, self::SEARCH_LIKE)) {
+                $sql .= "$key LIKE :$key AND ";
+                $values[] = [
+                    'param' => ":$key",
+                    'value' => '%'.$val.'%',
+                    'type' => \PDO::PARAM_STR,
+                ];
+            } else if (in_array($key, self::SEARCH_AND)) {
+                $sql .= "$key = :$key AND ";
+                $values[] = [
+                    'param' => ":$key",
+                    'value' => $val,
+                    'type' => \PDO::PARAM_STR,
+                ];
+            }
         }
+
         str_ends_with($sql, 'WHERE ')
             ? $sql = substr($sql, 0, -6)
             : $sql = substr($sql, 0, -4);
+
         $sql .= 'ORDER BY RAND() LIMIT 25';
 
         $arr = Pdo::getInstance($this->getParameter('pdo'))
