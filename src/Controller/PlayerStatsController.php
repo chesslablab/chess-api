2fa2c6f7fd91da40165de0ffafe36c34a8aac353
programarivm <info@programarivm.com>
2022-09-05 11:29:02 +0200

    Implemented PlayerStatsController

---
+++ src/Controller/PlayerStatsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ChessApi\Controller;
+
+use ChessApi\Pdo;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PlayerStatsController extends AbstractController
+{
+    const SEARCH_LIKE = [
+        'White',
+        'Black',
+    ];
+
+    const SEARCH_AND = [
+        'Result',
+    ];
+
+    public function index(Request $request): Response
+    {
+        $params = json_decode($request->getContent(), true);
+
+        $sql = 'SELECT ECO, COUNT(*) as total FROM players WHERE ';
+        $values = [];
+
+        foreach ($params as $key => $val) {
+            if (in_array($key, self::SEARCH_LIKE)) {
+                $sql .= "$key LIKE :$key AND ";
+                $values[] = [
+                    'param' => ":$key",
+                    'value' => '%'.$val.'%',
+                    'type' => \PDO::PARAM_STR,
+                ];
+            } else if (in_array($key, self::SEARCH_AND) && $val) {
+                $sql .= "$key = :$key AND ";
+                $values[] = [
+                    'param' => ":$key",
+                    'value' => $val,
+                    'type' => \PDO::PARAM_STR,
+                ];
+            }
+        }
+
+        str_ends_with($sql, 'WHERE ')
+            ? $sql = substr($sql, 0, -6)
+            : $sql = substr($sql, 0, -4);
+
+        $sql .= 'GROUP BY ECO ORDER BY total DESC';
+
+        $arr = Pdo::getInstance($this->getParameter('pdo'))
+            ->query($sql, $values)
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        if ($arr) {
+            return $this->json($arr);
+        }
+
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_NO_CONTENT);
+
+        return $response;
+    }
+}
