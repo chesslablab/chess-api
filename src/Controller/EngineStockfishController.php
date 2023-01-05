e77d60a9555e88a6d4640e3d9464816a84553dc9
programarivm <info@programarivm.com>
2023-01-05 11:09:54 +0100

    Implemented EngineStockfishController

---
+++ src/Controller/EngineStockfishController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace ChessApi\Controller;
+
+use Chess\Player\LanPlayer;
+use Chess\UciEngine\Stockfish;
+use Chess\Variant\Classical\Board as ClassicalBoard;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class EngineStockfishController extends AbstractController
+{
+    public function index(Request $request): Response
+    {
+        $params = json_decode($request->getContent(), true);
+
+        if (!isset($params['movetext'])) {
+            throw new BadRequestHttpException();
+        }
+
+        if (!isset($params['skillLevel'])) {
+            throw new BadRequestHttpException();
+        }
+
+        if (!isset($params['depth'])) {
+            throw new BadRequestHttpException();
+        }
+
+        try {
+            $board = (new LanPlayer($params['movetext'], new ClassicalBoard()))
+                ->play()->getBoard();
+
+            $stockfish = (new Stockfish($board))
+                ->setOptions([
+                    'Skill Level' => $params['skillLevel'],
+                ])
+                ->setParams([
+                    'depth' => $params['depth'],
+                ]);
+
+            $lan = $stockfish->play($board->toFen());
+        } catch (\Exception $e) {
+            throw new BadRequestHttpException();
+        }
+
+        $arr = [
+            'move' => $lan,
+        ];
+
+        return $this->json($arr);
+    }
+}
