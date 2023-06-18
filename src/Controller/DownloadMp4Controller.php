03eb45386a48b1350f708cda66940150fb1d5b57
programarivm <info@programarivm.com>
2023-06-18 16:49:17 +0200

    Updated to PHP Chess 1.3.06

--- src/Controller/DownloadMp4Controller.php
+++ src/Controller/DownloadMp4Controller.php
@@ -3,8 +3,12 @@
 namespace ChessApi\Controller;
 
 use Chess\Media\BoardToMp4;
+use Chess\Variant\Capablanca\Board as CapablancaBoard;
+use Chess\Variant\Capablanca\FEN\StrToBoard as CapablancaFenStrToBoard;
 use Chess\Variant\Chess960\Board as Chess960Board;
-use Chess\Variant\Classical\PGN\AN\Color;
+use Chess\Variant\Chess960\FEN\StrToBoard as Chess960FenStrToBoard;
+use Chess\Variant\Classical\Board as ClassicalBoard;
+use Chess\Variant\Classical\FEN\StrToBoard as ClassicalFenStrToBoard;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -36,13 +40,37 @@ class DownloadMp4Controller extends AbstractController
         }
 
         try {
+            if ($params['fen']) {
+                if ($params['variant'] === Chess960Board::VARIANT) {
+                    $board = (new Chess960FenStrToBoard($params['fen'], $params['startPos']))
+                        ->create();
+                } elseif ($params['variant'] === CapablancaBoard::VARIANT) {
+                    $board = (new CapablancaFenStrToBoard($params['fen']))
+                        ->create();
+                } elseif ($params['variant'] === ClassicalBoard::VARIANT) {
+                    $board = (new ClassicalFenStrToBoard($params['fen']))
+                        ->create();
+                } else {
+                    throw new BadRequestHttpException();
+                }
+            } else {
+                if ($params['variant'] === Chess960Board::VARIANT) {
+                    $board = new Chess960Board($params['startPos']);
+                } elseif ($params['variant'] === CapablancaBoard::VARIANT) {
+                    $board = new CapablancaBoard();
+                } elseif ($params['variant'] === ClassicalBoard::VARIANT) {
+                    $board = new ClassicalBoard();
+                } else {
+                    throw new BadRequestHttpException();
+                }
+            }
+
             $filename = (new BoardToMp4(
-                $params['variant'],
                 $params['movetext'],
-                $params['fen'] ?? '',
-                $params['startPos'] ?? '',
-                $params['flip'] === Color::B
+                $board,
+                $flip = $params['flip']
             ))->output(self::OUTPUT_FOLDER);
+
             $request->attributes->set('filename', $filename);
         } catch (\Exception $e) {
             return (new Response())->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
