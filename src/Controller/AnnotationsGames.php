8208e19ff10ea40e94b24f654f3540d33bf13d5b
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-07-28 16:22:15 +0100

    Update AnnotationsGames.php

--- src/Controller/AnnotationsGames.php
+++ src/Controller/AnnotationsGames.php
@@ -5,3 +5,8 @@ namespace ChessApi\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
+class AnnotationsGames extends AbstractController
+{
+  const DATA_FOLDER = __DIR__.'/../../ data/annotations/games.json';
+}
