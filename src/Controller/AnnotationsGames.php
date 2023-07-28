854e7610f9dfab64dae224bdc2a1c19dcfbd0996
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-07-28 16:27:30 +0100

    Update AnnotationsGames.php with full code

--- src/Controller/AnnotationsGames.php
+++ src/Controller/AnnotationsGames.php
@@ -9,4 +9,22 @@ use Symfony\Component\HttpFoundation\Response;
 class AnnotationsGames extends AbstractController
 {
   const DATA_FOLDER = __DIR__.'/../../ data/annotations/';
+
+  const ANNOTATIONS_GAMES_FILE = 'games.json';
+
+  public function index(Request $request): Response
+    {
+        $games = file_get_contents(self::DATA_FOLDER.'/'.self::ANNOTATIONS_GAMES_FILE);
+
+        $content = json_encode([
+            'games' => json_decode($games),
+        ]);
+
+        $response = new Response();
+        $response->setContent($content);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+  
 }
