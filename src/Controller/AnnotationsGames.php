be18fe307abfd94eedbd21f9c5bc619f3f80c181
SmashedFrenzy16 <smashedfrenzy16@gmail.com>
2023-07-28 16:19:58 +0100

    Update AnnotationsGames.php

--- src/Controller/AnnotationsGames.php
+++ src/Controller/AnnotationsGames.php
@@ -1,1 +1,7 @@
+<?php
 
+namespace ChessApi\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
