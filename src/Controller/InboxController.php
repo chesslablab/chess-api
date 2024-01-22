c8bfda722b884f989e45f6503c309b78e0092683
programarivm <info@programarivm.com>
2024-01-22 17:37:08 +0100

    Implemented Implemented POST /api/inbox/reply

--- src/Controller/InboxController.php
+++ src/Controller/InboxController.php
@@ -2,6 +2,8 @@
 
 namespace ChessApi\Controller;
 
+use Chess\Exception\MovetextException;
+use Chess\Movetext\SanMovetext;
 use Chess\Variant\Capablanca\Board as CapablancaBoard;
 use Chess\Variant\Capablanca\FEN\StrToBoard as CapablancaFenStrToBoard;
 use Chess\Variant\Capablanca\PGN\Move as CapablancaPgnMove;
@@ -13,6 +15,7 @@ use Chess\Variant\Chess960\StartPosition as Chess960StartPosition;
 use Chess\Variant\Chess960\FEN\StrToBoard as Chess960FenStrToBoard;
 use Chess\Variant\Classical\Board as ClassicalBoard;
 use Chess\Variant\Classical\FEN\StrToBoard as ClassicalFenStrToBoard;
+use Chess\Variant\Classical\PGN\Move as ClassicalPgnMove;
 use ChessApi\Pdo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,6 +24,12 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class InboxController extends AbstractController
 {
+    const ACTION_CREATE = 'create';
+
+    const ACTION_READ = 'read';
+
+    const ACTION_REPLY = 'reply';
+
     public function create(Request $request): Response
     {
         $params = json_decode($request->getContent(), true);
@@ -29,9 +38,7 @@ class InboxController extends AbstractController
             throw new BadRequestHttpException();
         }
 
-        $settings = isset($params['settings'])
-            ? json_decode(stripslashes($params['settings']), true)
-            : '{}';
+        $settings = $params['settings'] ?? [];
 
         $hash = md5(uniqid());
 
@@ -71,7 +78,7 @@ class InboxController extends AbstractController
             ],
             [
                 'param' => ':settings',
-                'value' => $settings,
+                'value' => json_encode($settings, true),
                 'type' => \PDO::PARAM_STR
             ],
             [
@@ -130,6 +137,125 @@ class InboxController extends AbstractController
     {
         $params = json_decode($request->getContent(), true);
 
-        print_r($params); exit;
+        if (!isset($params['hash'])) {
+            throw new BadRequestHttpException();
+        }
+        if (!isset($params['movetext'])) {
+            throw new BadRequestHttpException();
+        }
+
+        $sql = 'SELECT * FROM inbox WHERE hash = :hash';
+
+        $values = [
+            [
+                'param' => ':hash',
+                'value' => $params['hash'],
+                'type' => \PDO::PARAM_STR
+            ],
+        ];
+
+        $arr = Pdo::getInstance($this->getParameter('pdo'))
+            ->query($sql, $values)
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        if ($arr) {
+            $inbox = current($arr);
+            $settings = json_decode($inbox['settings'], true);
+
+            if (isset($settings['fen'])) {
+                if ($inbox['variant'] === Chess960Board::VARIANT) {
+                   $move = new ClassicalPgnMove();
+                   $startPos = str_split($inbox['settings']['startPos']);
+                   $board = (new Chess960FenStrToBoard($inbox['settings']['fen'], $startPos))
+                       ->create();
+               } elseif ($inbox['variant'] === CapablancaBoard::VARIANT) {
+                   $move = new CapablancaPgnMove();
+                   $board = (new CapablancaFenStrToBoard($inbox['settings']['fen']))
+                       ->create();
+               } elseif ($inbox['variant'] === CapablancaFischerBoard::VARIANT) {
+                   $move = new CapablancaPgnMove();
+                   $startPos = str_split($inbox['settings']['startPos']);
+                   $board = (new CapablancaFischerFenStrToBoard($inbox['settings']['fen'], $startPos))
+                       ->create();
+               } else {
+                   $move = new ClassicalPgnMove();
+                   $board = (new ClassicalFenStrToBoard($inbox['settings']['fen']))
+                       ->create();
+               }
+            } else {
+                if ($inbox['variant'] === Chess960Board::VARIANT) {
+                    $move = new ClassicalPgnMove();
+                    $startPos = (new Chess960StartPosition())->create();
+                    $board = new Chess960Board($startPos);
+                } elseif ($inbox['variant'] === CapablancaBoard::VARIANT) {
+                    $move = new CapablancaPgnMove();
+                    $board = new CapablancaBoard();
+                } elseif ($inbox['variant'] === CapablancaFischerBoard::VARIANT) {
+                    $move = new CapablancaPgnMove();
+                    $startPos = (new CapablancaFischerStartPosition())->create();
+                    $board = new CapablancaFischerBoard($startPos);
+                } else {
+                    $move = new ClassicalPgnMove();
+                    $board = new ClassicalBoard();
+                }
+            }
+
+            try {
+                if ($inbox['movetext']) {
+                    $san = new SanMovetext($move, $inbox['movetext']);
+                    $san->validate();
+                    foreach ($san->getMoves() as $key => $val) {
+                        $board->play($board->getTurn(), $val);
+                    }
+                }
+                if (!$board->play($board->getTurn(), $params['movetext'])) {
+                    throw new MovetextException();
+                }
+
+                $sql = "UPDATE inbox
+                    SET fen = :fen, movetext = :movetext, updatedAt = :updatedAt
+                WHERE hash = :hash";
+
+                $values = [
+                    [
+                        'param' => ':fen',
+                        'value' => $board->toFen(),
+                        'type' => \PDO::PARAM_STR
+                    ],
+                    [
+                        'param' => ':movetext',
+                        'value' => $board->getMovetext(),
+                        'type' => \PDO::PARAM_STR
+                    ],
+                    [
+                        'param' => ':updatedAt',
+                        'value' => (new \DateTime())->format('Y-m-d H:i:s'),
+                        'type' => \PDO::PARAM_STR
+                    ],
+                    [
+                        'param' => ':hash',
+                        'value' => $params['hash'],
+                        'type' => \PDO::PARAM_STR
+                    ],
+                ];
+
+                Pdo::getInstance($this->getParameter('pdo'))->query($sql, $values);
+
+                return $this->json([
+                    'action' => InboxController::ACTION_REPLY,
+                    'message' =>  'Chess move successfully sent.',
+                ]);
+            } catch (\Exception $e) {
+                return $this->json([
+                    'action' => InboxController::ACTION_REPLY,
+                    'message' =>  'Invalid PGN move, please try again with a different one.',
+                ]);
+            }
+        }
+
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_NO_CONTENT);
+
+        return $response;
     }
 }
