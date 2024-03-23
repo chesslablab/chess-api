1f258503104de3e263470074d0be43013bd14c4c
programarivm <info@programarivm.com>
2024-03-23 17:20:39 +0100

    Implemented endpoint

--- src/Controller/AutocompleteEventController.php
+++ src/Controller/AutocompleteEventController.php
@@ -2,23 +2,41 @@
 
 namespace ChessApi\Controller;
 
+use ChessApi\Pdo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AutocompleteEventController extends AbstractController
 {
-    const DATA_FOLDER = __DIR__.'/../../data/autocomplete';
-
-    const AUTOCOMPLETE_EVENT_FILE = 'events.json';
-
     public function index(Request $request): Response
     {
-        $content = file_get_contents(self::DATA_FOLDER.'/'.self::AUTOCOMPLETE_EVENT_FILE);
+        $params = json_decode($request->getContent(), true);
+
+        if (!isset($params['Event'])) {
+            throw new BadRequestHttpException();
+        }
+
+        $key = key($params);
+
+        $values[] = [
+            'param' => ":$key",
+            'value' => '%'. $params['Event'] .'%',
+            'type' => \PDO::PARAM_STR,
+        ];
+
+        $sql = "SELECT * FROM games WHERE $key LIKE :$key ORDER BY RAND() LIMIT 10";
+
+        $arr = Pdo::getInstance($this->getParameter('pdo'))
+            ->query($sql, $values)
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        if ($arr) {
+            return $this->json($arr);
+        }
 
         $response = new Response();
-        $response->setContent($content);
-        $response->headers->set('Content-Type', 'application/json');
+        $response->setStatusCode(Response::HTTP_NO_CONTENT);
 
         return $response;
     }
