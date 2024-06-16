4f375b3f08fded927fddf8ba8fff0c0d4d02bcbe
programarivm <info@programarivm.com>
2024-06-16 13:47:22 +0200

    Replaced JSON files with SQL queries

--- src/Controller/StatsOpeningController.php
+++ src/Controller/StatsOpeningController.php
@@ -2,30 +2,55 @@
 
 namespace ChessApi\Controller;
 
+use ChessApi\Pdo;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class StatsOpeningController extends AbstractController
 {
-    const DATA_FOLDER = __DIR__.'/../../data/stats/opening';
-
-    const DRAW_RATE_FILE = 'draw-rate.json';
-
-    const WIN_RATE_FOR_WHITE_FILE = 'win-rate-for-white.json';
-
-    const WIN_RATE_FOR_BLACK_FILE = 'win-rate-for-black.json';
-
     public function index(Request $request): Response
     {
-        $drawRate = file_get_contents(self::DATA_FOLDER.'/'.self::DRAW_RATE_FILE);
-        $winRateForWhite = file_get_contents(self::DATA_FOLDER.'/'.self::WIN_RATE_FOR_WHITE_FILE);
-        $winRateForBlack = file_get_contents(self::DATA_FOLDER.'/'.self::WIN_RATE_FOR_BLACK_FILE);
+        $sql = "SELECT ECO, COUNT(*) AS total
+            FROM games
+            WHERE Result = '1/2-1/2'
+            GROUP BY ECO
+            HAVING total >= 100
+            ORDER BY total DESC
+            LIMIT 50";
+
+        $drawRate = Pdo::getInstance($this->getParameter('pdo'))
+            ->query($sql)
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        $sql = "SELECT ECO, COUNT(*) AS total
+            FROM games
+            WHERE Result = '1-0'
+            GROUP BY ECO
+            HAVING total >= 100
+            ORDER BY total DESC
+            LIMIT 50";
+
+        $winRateForWhite = Pdo::getInstance($this->getParameter('pdo'))
+            ->query($sql)
+            ->fetchAll(\PDO::FETCH_ASSOC);
+
+        $sql = "SELECT ECO, COUNT(*) AS total
+            FROM games
+            WHERE Result = '0-1'
+            GROUP BY ECO
+            HAVING total >= 100
+            ORDER BY total DESC
+            LIMIT 50";
+
+        $winRateForBlack = Pdo::getInstance($this->getParameter('pdo'))
+            ->query($sql)
+            ->fetchAll(\PDO::FETCH_ASSOC);
 
         $content = json_encode([
-            'drawRate' => json_decode($drawRate),
-            'winRateForWhite' => json_decode($winRateForWhite),
-            'winRateForBlack' => json_decode($winRateForBlack)
+            'drawRate' => $drawRate,
+            'winRateForWhite' => $winRateForWhite,
+            'winRateForBlack' => $winRateForBlack
         ]);
 
         $response = new Response();
